ff5f91a076fe8ab7b94e16a43fba498cc86e1bf9
qbjsdsb <3342513714@qq.com>
2026-09-15 19:02:38 +0800

    feat: add table configuration

---
+++ src/config/table.js
@@ -0,0 +1,29 @@
+export const TABLE = Object.freeze({
+  length: 3.569,
+  width: 1.778,
+  ballDiameter: 0.0525,
+  ballRadius: 0.02625,
+  baulkDistance: 0.737,
+  dRadius: 0.292,
+  cushionHeight: 0.055,
+  railWidth: 0.115,
+  cornerPocketRadius: 0.052,
+  middlePocketRadius: 0.055,
+  cornerGap: 0.082,
+  middleGap: 0.105,
+  jawRadius: 0.016,
+});
+
+export const HALF_L = TABLE.length / 2;
+export const HALF_W = TABLE.width / 2;
+
+export const BALL_COLORS = Object.freeze({
+  cue: 0xf4f3ea,
+  red: 0xb72327,
+  yellow: 0xe8c438,
+  green: 0x2c8a4b,
+  brown: 0x7b4b2b,
+  blue: 0x2f65b7,
+  pink: 0xd784aa,
+  black: 0x171717,
+});
